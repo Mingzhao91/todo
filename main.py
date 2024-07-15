e64bf59a95bcba1723381cf14781364d11b48734
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-07-15 21:56:40 +0100

    store todos in file

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
-            todos.append(todo.capitalize())
+            todo = input("Enter a todo: ") + "\n"
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            todos.append(todo)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
         case "show":
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
             for index, todo in enumerate(todos):
-                print(f"{index + 1} -  {todo}")
+                print(f"{index + 1}-{todo}".rstrip())
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
             new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + "\n"
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
         case "complete":
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            todo_to_remove = todos.pop(number - 1)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove.rstrip()} was removed from the list"
+
+            print(message)
         case "exit":
             break
         case _:
