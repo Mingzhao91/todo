750f57e544bc803e176ead04de63679048d59fbe
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-07-19 14:15:20 +0100

    add doc string

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """
+    Read a text file and return the list of to-do items.
+    """
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write the to-do items in the text file. """
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
 
@@ -15,12 +19,12 @@ while True:
 
     if user_action.startswith("add"):
         todo = user_action[4:]
-        todos = get_todos("todos.txt")
+        todos = get_todos()
         todos.append(todo + '\n')
-        write_todos("todos.txt", todos)
+        write_todos( todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         for index, todo in enumerate(todos):
             print(f"{index + 1}-{todo}".rstrip())
@@ -29,10 +33,10 @@ while True:
         try:
             number = int(user_action[5:])
             number = number - 1
-            todos = get_todos("todos.txt")
+            todos = get_todos()
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo + "\n"
-            write_todos("todos.txt", todos)
+            write_todos( todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -40,9 +44,9 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos("todos.txt")
+            todos = get_todos()
             todo_to_remove = todos.pop(number - 1)
-            write_todos("todos.txt", todos)
+            write_todos( todos)
             message = f"Todo {todo_to_remove.rstrip()} was removed from the list"
 
             print(message)
