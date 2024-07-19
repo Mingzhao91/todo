80f5c982c56016c56cb7e454aa5cadc5c4d1eab8
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-07-19 15:03:30 +0100

    put functions into a module

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of to-do items.
-    """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items in the text file. """
-    with open(filepath, "w") as file_local:
-        file_local.writelines(todos_arg)
-
+from modules.functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -21,7 +8,7 @@ while True:
         todo = user_action[4:]
         todos = get_todos()
         todos.append(todo + '\n')
-        write_todos( todos)
+        write_todos(todos)
 
     elif user_action.startswith("show"):
         todos = get_todos()
@@ -36,7 +23,7 @@ while True:
             todos = get_todos()
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo + "\n"
-            write_todos( todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -46,7 +33,7 @@ while True:
             number = int(user_action[9:])
             todos = get_todos()
             todo_to_remove = todos.pop(number - 1)
-            write_todos( todos)
+            write_todos(todos)
             message = f"Todo {todo_to_remove.rstrip()} was removed from the list"
 
             print(message)
