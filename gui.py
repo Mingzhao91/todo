1eb26584dea8ddde23211497b7cad6572941ea8e
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-07-20 16:20:21 +0100

    implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -6,9 +6,17 @@ input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=get_todos(), key="todos", enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+layout = [
+    [label],
+    [input_box, add_button],
+    [list_box, edit_button, complete_button],
+    [exit_button]
+]
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button],],
+                   layout=layout,
                    font=("Helvetica", 20))
 
 while True:
@@ -24,7 +32,7 @@ while True:
             todos.append(new_todo)
             write_todos(todos)
             # update the list after the new item is added
-            window["todos"].Update(values=todos)
+            window["todos"].update(values=todos)
         case "Edit":
             todo_to_edit = values["todos"][0]
             new_todo = values['todo']
@@ -33,10 +41,19 @@ while True:
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             write_todos(todos)
-            window['todos'].Update(values=todos)
+            window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = get_todos()
+            todos.remove(todo_to_complete)
+            write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             # update the input when list item is clicked
-            window['todo'].update(values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
             # click the close button
             break
