c219b46d1bc19b5302724eae2092d6cc44b7b1e4
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-07-20 20:30:28 +0100

    create a sttic web interface

---
+++ web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from modules.functions import get_todos, write_todos
+
+todos = get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add new todo...")
